46f8fc859f8eff1aceb85230791e8edd5d1f91f3
erpn900 <erp.n900@gmail.com>
2026-05-19 08:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SCHOOL_APP_CONFIG = {
   // Paste your Google Apps Script Web App URL here after deployment.
   // Example: "https://script.google.com/macros/s/AKfycb.../exec"
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby7cA5jecgEFDjMuzp8HvdG_dpZguJ8lVGAQ3UXD3EVpkedN4ssLvH5gWJWsDQb66S_/exec",
 
   // Optional. If you set Script Property APP_TOKEN in Apps Script, paste the same value here.
   APP_TOKEN: "",
